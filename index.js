2f4a64af264145e313327eafdf22ef0f97829de5
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-14 19:58:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
